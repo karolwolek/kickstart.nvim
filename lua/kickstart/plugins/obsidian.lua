5e187fffc638d7702170800c73bdc00c09f6e556
karolwolek <karolwolek06@gmail.com>
2025-06-11 14:55:56 +0200

    standardize obsidian file

--- lua/kickstart/plugins/obsidian.lua
+++ lua/kickstart/plugins/obsidian.lua
@@ -1,13 +1,19 @@
 return {
   {
     'obsidian-nvim/obsidian.nvim',
+
+    -- TODO: track release for resolved issues with folds etc.
     tag = 'v3.11.0',
-    -- lazy = true,
+
+    -- attach obsidian only when entering one of the vault's buffers
     event = {
       'BufEnter ' .. vim.fn.expand '~' .. '/Documents/myvault/*.md',
       'BufNewFile ' .. vim.fn.expand '~' .. '/Documents/myvault/*.md',
     },
+
+    -- attach obsidian on one of the commands call
     cmd = 'Obsidian',
+
     opts = {
       workspaces = {
         {
@@ -16,14 +22,24 @@ return {
         },
       },
       log_level = vim.log.levels.INFO,
+
+      -- [[ new notes location; inbox system ]]
       notes_subdir = 'inbox/',
       new_notes_location = 'notes_subdir',
+
+      -- [[ my custom field for trash]]
+      --
+      -- The reason for this is to have some place to put discarded note into
+      -- rather than deleting it. It's a safety feature for unwanted actions.
       trash_dir = '.trash',
+
+      -- [[ use blink and fire it immediately ]]
       completion = {
         blink = true,
         nvim_cmp = false,
         min_chars = 0,
       },
+      -- [[ configure daily notes with default template ]]
       daily_notes = {
         folder = 'notes/dailies',
         date_format = '%Y-%m-%d',
@@ -32,11 +48,25 @@ return {
         template = 'daily.md',
         workdays_only = true,
       },
+
+      -- [[ templates location and placeholders]]
+      --
+      -- I can add some custom placeholders
       templates = {
         folder = 'templates',
         date_format = '%Y-%m-%d',
         time_format = '%H:%M',
       },
+
+      -- [[ mappings ]]
+      --
+      -- Some mappings get attached to a certain buffer,
+      -- some mappings are using my custom obutils for several note taking system parts.
+      --
+      -- The reason they are defined here is that obsidian
+      -- is managing autocmds with buffer attached mappings itself
+      -- so i don't need to deal with checking whether the buffer
+      -- is a part of the workspace or not
       mappings = {
         -- Toggle check-boxes.
         ['<leader>ch'] = {
@@ -96,12 +126,12 @@ return {
           opts = { buffer = true, expr = false, noremap = true, desc = '[N]ote [A]ccept' },
         },
         -- paste image without default name
-        ['<M-P>'] = {
+        ['<M-p>'] = {
           action = require('kickstart.obutils').paste_image_custom,
           opts = { buffer = true, expr = false, noremap = true, desc = '[P]aste image without default' },
         },
         -- paste image with default name
-        ['<M-p>'] = {
+        ['<M-P>'] = {
           action = require('kickstart.obutils').paste_image_default,
           opts = { buffer = true, expr = false, noremap = true, desc = '[P]aste image with default name' },
         },
@@ -127,8 +157,12 @@ return {
           opts = { buffer = false, expr = true, noremap = true, desc = '[N]ote [T]oday' },
         },
       },
-      ---@param title string|?
-      ---@return string
+
+      -- [[ note naming function]]
+      --
+      -- It attaches the timestamp to each note with pattern 2004-06-16.
+      -- If note name is not specified the title is random ( I do not use it since
+      -- my custom functions require the title).
       note_id_func = function(title)
         local suffix = ''
         if title ~= nil then
@@ -142,72 +176,79 @@ return {
         end
         return tostring(os.date '%Y-%m-%d') .. '_' .. suffix
       end,
-      wiki_link_func = 'prepend_note_path',
-      -- Optional, customize how markdown links are formatted.
-      markdown_link_func = function(opts)
-        return require('obsidian.util').markdown_link(opts)
-      end,
-      preferred_link_style = 'wiki',
-      disable_frontmatter = false,
 
-      -- Optional, alternatively you can customize the frontmatter data.
-      ---@return table
-      note_frontmatter_func = function(note)
-        -- Add the title of the note as an alias.
-        if note.title then
-          note:add_alias(note.title)
-        end
+      -- [[links customization]]
+      wiki_link_func = 'prepend_note_path', -- meaning e.g. '[[foo-bar.md|Foo Bar]]'
+      preferred_link_style = 'markdown', -- Either 'wiki' or 'markdown'.
 
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and not vim.tbl_isempty(note.metadata) then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
-          end
-        end
-
-        return out
-      end,
-
+      -- [[ open links in firefox ]]
       follow_url_func = function(url)
-        vim.fn.jobstart { 'xdg-open', url } -- linux
+        vim.ui.open(url, { cmd = { 'firefox' } })
       end,
 
+      -- [[ open images as a regular buffer ]]
       follow_img_func = function(img)
-        vim.fn.jobstart { 'xdg-open', img } -- linux
+        vim.cmd { cmd = 'edit', args = { img } }
       end,
 
+      -- [[ callbacks]]
+      --
+      -- Various actions for scripting during obsidian lifecycle.
+      --
+      -- Currently after entering the buffer i set custom `resolve` function for images
+      -- in snacks because of the relative to the vault link setup.
+      -- After leaving the note i schedule the job to nullify the `resolve`.
+      -- Scheduling allows me to avoid the situation while snacks is still trying to render
+      -- while i am interfering with config.
+      --
+      -- Second autocmds is for tag highlight. render-markdown is not detecting these properly
+      -- and i need to have obisidan ui off due to incompatibility issues.
       callbacks = {
-        enter_note = function(_, _)
+        ---@param client obsidian.Client
+        enter_note = function(client, _)
+          -- image snacks
+          require('snacks.image').config.resolve = function(_, src)
+            local workspace_path = client.current_workspace.path:resolve()
+            local img_path = workspace_path:joinpath(src):resolve()
+            return img_path.filename
+          end
           -- Tag highlighting
           vim.cmd 'highlight myTag guifg=#71d4eb'
           vim.cmd 'match myTag /#[0-9]*[a-zA-Z_\\-\\/][a-zA-Z_\\-\\/0-9]*/'
         end,
         leave_note = function(_, _)
+          -- image snacks
+          vim.schedule(function()
+            require('snacks.image').config.resolve = nil
+          end)
           -- Tag highlighting
           vim.cmd 'highlight clear myTag'
         end,
       },
-      -- Disable the obsidian UI because of the incompatibility with render-markdown plugin
+
+      -- [[ UI ]]
+      --
+      -- Disable the obsidian UI because of the incompatibility
+      -- with render-markdown plugin
       ui = {
         enable = false,
       },
 
+      -- [[ attachments ]]
+      --
+      -- Set default images folder,
+      -- disable pasting confirmation and set custom filename function
+      -- that properly renames the file to attach timestamps and prepares
+      -- a string with just the name to inject in the text with a pattern:
+      -- [this is example](images/this-is-example-20250616145425.jpg)
       attachments = {
         img_folder = '/home/karolwolek/Documents/myvault/images/',
         confirm_img_paste = false,
-        img_name_func = function()
-          return string.format('Pasted image %s', os.date '%Y%m%d%H%M%S')
-        end,
-
-        ---@param client obsidian.Client
-        ---@param path obsidian.Path the absolute path to the image file
-        ---@return string
         img_text_func = function(client, path)
           local name = path.stem
-          local target_file_name = string.gsub(path.filename, ' ', '-')
+          local path_with_timestamp = path.new(path:parent().filename)
+          path_with_timestamp = path_with_timestamp:joinpath(path.stem .. os.date '-%Y%m%d%H%M%S' .. path.suffix)
+          local target_file_name = string.gsub(path_with_timestamp.filename, ' ', '-')
           local success, err = pcall(function()
             os.rename(path.filename, target_file_name)
           end)
@@ -219,6 +260,11 @@ return {
           return string.format('![%s](%s)', name, path)
         end,
       },
+
+      -- [[ status line integration ]]
+      --
+      -- This custom status line gets transferred to mini.statusline
+      -- and is handled there accordingly.
       statusline = {
         enabled = true,
         format = '{{backlinks}} backlinks  {{properties}} properties  {{words}} words  {{chars}} chars',
